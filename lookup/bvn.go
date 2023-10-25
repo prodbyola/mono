00427e5db0996a6ecfe7a35a7c03432cc668ccb3
Ajayi Olamide Daniel <prodbyola@Ajayis-MacBook-Pro.local>
2023-10-25 16:21:07 +0100

    Added Bvn LookUp 'Fetch' API.

--- lookup/bvn.go
+++ lookup/bvn.go
@@ -13,14 +13,15 @@ type BvnAvailableMethod struct {
 type BvnVerificationMethod struct {
 }
 
-// BvnInitiationResponse represents the response structure for BVN initiation.
+// BvnVerificationResponse represents the response structure for BVN initiation.
 // It contains information about the status of the initiation, a message, available methods,
 // and a session ID.
-type BvnInitiationResponse struct {
-	Status    string               `json:"status"`
-	Message   string               `json:"message"`
-	Methods   []BvnAvailableMethod `json:"methods"`
-	SessionID string               `json:"session_id"`
+type BvnVerificationResponse struct {
+	Status  string                 `json:"status"`
+	Message string                 `json:"message"`
+	Data    map[string]interface{} `json:"data"`
+	// Methods   []BvnAvailableMethod `json:"methods"`
+	// SessionID string               `json:"session_id"`
 }
 
 // parse is a method of the BvnInitiationResponse struct that populates the struct's fields
@@ -39,34 +40,34 @@ type BvnInitiationResponse struct {
 //	response := BvnInitiationResponse{}
 //	data := map[string]interface{}{"status": "success", "message": "Initiated", ...}
 //	response.parse(data)
-func (r *BvnInitiationResponse) parse(d map[string]interface{}) {
+func (r *BvnVerificationResponse) parse(d map[string]interface{}) {
 	message := d["message"].(string) // message should always be a string
 	status := d["status"]
 	data := d["data"]
 
 	if data != nil {
-		data := data.(map[string]interface{})
-		sid := data["session_id"]
+		r.Data = data.(map[string]interface{})
+		// sid := data["session_id"]
 
-		m := data["methods"].([]interface{})
-		var methods []BvnAvailableMethod
+		// m := data["methods"].([]interface{})
+		// var methods []BvnAvailableMethod
 
-		// Let's convert a `[]interface{}` to []BvnVerificationMethod
-		for i := 0; i < len(m); i++ {
-			mtds := m[i].(map[string]interface{})
-			nm := BvnAvailableMethod{
-				Method: mtds["method"].(string),
-				Hint:   mtds["hint"].(string),
-			}
+		// // Let's convert a `[]interface{}` to []BvnVerificationMethod
+		// for i := 0; i < len(m); i++ {
+		// 	mtds := m[i].(map[string]interface{})
+		// 	nm := BvnAvailableMethod{
+		// 		Method: mtds["method"].(string),
+		// 		Hint:   mtds["hint"].(string),
+		// 	}
 
-			methods = append(methods, nm)
-		}
+		// 	methods = append(methods, nm)
+		// }
 
-		if sid != nil {
-			r.SessionID = sid.(string)
-		}
+		// if sid != nil {
+		// 	r.SessionID = sid.(string)
+		// }
 
-		r.Methods = methods
+		// r.Methods = methods
 	}
 
 	r.Message = message
@@ -75,12 +76,34 @@ func (r *BvnInitiationResponse) parse(d map[string]interface{}) {
 	}
 }
 
-func (r *BvnInitiationResponse) IsSuccessful() bool {
+func (r *BvnVerificationResponse) SessionID() string {
+	return r.Data["session_id"].(string)
+}
+
+func (r *BvnVerificationResponse) IsSuccessful() bool {
 	return r.Status == "successful"
 }
 
-func (r *BvnInitiationResponse) NotSuccessful() bool {
+func (r *BvnVerificationResponse) NotSuccessful() bool {
 	return !r.IsSuccessful()
+}
+
+func (r *BvnVerificationResponse) AvailableMethods() []BvnAvailableMethod {
+	m := r.Data["methods"].([]interface{})
+	var methods []BvnAvailableMethod
+
+	// Let's convert a `[]interface{}` to []BvnVerificationMethod
+	for i := 0; i < len(m); i++ {
+		mtds := m[i].(map[string]interface{})
+		nm := BvnAvailableMethod{
+			Method: mtds["method"].(string),
+			Hint:   mtds["hint"].(string),
+		}
+
+		methods = append(methods, nm)
+	}
+
+	return methods
 }
 
 // BvnLookUp represents a client for initiating and verifying BVN (Bank Verification Number) requests.
@@ -108,9 +131,9 @@ func NewBvnLookUp(apiKey string) BvnLookUp {
 //   - BvnInitiationResponse: The response from the BVN initiation request.
 //
 // Check Step 1 on https://docs.mono.co/docs/bvn-lookup-integration-guide for more info.
-func (l *BvnLookUp) Initiate(bvn string) BvnInitiationResponse {
+func (l *BvnLookUp) Initiate(bvn string) BvnVerificationResponse {
 	url := mono.BASE_URL + "lookup/bvn/initiate"
-	var resp BvnInitiationResponse
+	var resp BvnVerificationResponse
 
 	data := map[string]string{
 		"bvn": bvn,
@@ -137,6 +160,8 @@ func (l *BvnLookUp) Initiate(bvn string) BvnInitiationResponse {
 
 // Verifies a BVN with the specified verification method, session ID, and alternate phone number.
 //
+// Responsible for sending otp to the bvn holder based on their choice of method/channel.
+//
 // Parameters:
 //   - method: A BvnVerificationMethodType representing the verification method to be used.
 //   - session_id: A string representing the session ID for the verification request.
@@ -146,9 +171,9 @@ func (l *BvnLookUp) Initiate(bvn string) BvnInitiationResponse {
 //   - BvnInitiationResponse: The response from the BVN verification request.
 //
 // Check Step 2 on https://docs.mono.co/docs/bvn-lookup-integration-guide for more info.
-func (l *BvnLookUp) Verify(method mono.BvnVerificationMethodType, session_id string, alt_phone interface{}) BvnInitiationResponse {
+func (l *BvnLookUp) Verify(method mono.BvnVerificationMethodType, session_id string, alt_phone interface{}) BvnVerificationResponse {
 	url := mono.BASE_URL + "lookup/bvn/verify"
-	var resp BvnInitiationResponse
+	var resp BvnVerificationResponse
 
 	data := map[string]string{
 		"method": method.String(),
@@ -182,8 +207,46 @@ func (l *BvnLookUp) Verify(method mono.BvnVerificationMethodType, session_id str
 		resp.parse(d)
 	}
 
-	if resp.IsSuccessful() {
-		resp.SessionID = session_id
+	return resp
+}
+
+// Retrieves BVN details using a one-time password (OTP) and a session ID.
+//
+// This method sends a request to the API to fetch BVN details based on the provided OTP and session ID.
+//
+// Parameters:
+// - otp: A string representing the one-time password (OTP) for BVN verification.
+// - session_id: A string representing the session ID associated with the verification request.
+//
+// Returns:
+// - BvnVerificationResponse: The response containing BVN details, such as name, date of birth, etc.
+//
+// Note: Make sure to handle potential errors and check the response for success before using the details.
+func (l *BvnLookUp) FetchDetails(otp string, session_id string) BvnVerificationResponse {
+	url := mono.BASE_URL + "lookup/bvn/details"
+	var resp BvnVerificationResponse
+
+	data := map[string]string{
+		"otp": otp,
+	}
+
+	d, err := internal.MakeRequest[map[string]interface{}](internal.RequestConfig{
+		Url:    url,
+		Method: "POST",
+		Data:   data,
+		ApiKey: l.apiKey,
+		Headers: map[string]string{
+			"x-session-id": session_id,
+		},
+	})
+
+	if err != nil {
+		resp.Message = err.Error()
+		return resp
+	}
+
+	if d != nil {
+		resp.parse(d)
 	}
 
 	return resp
